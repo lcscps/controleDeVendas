9b1e46d1e1f17ed923b72f0f9619041580aa03c4
Lucas do Nascimento <lucascps07@gmail.com>
2020-03-10 11:58:31 -0300

    Criado a estrutura do projeto e suas dependencias

---
+++ app.js
@@ -0,0 +1,46 @@
+// Carregar modulos
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const path = require('path')
+const flash = require('connect-flash')
+const mongoose = require('mongoose')
+
+// Configurações
+// Session
+app.use(session({
+    secret: 'nodejs',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+// Middleware
+app.use((req,res,next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
+// Handlebars
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+// BodyParser
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+// Public
+app.use(express.static(path.join(__dirname, "public")))
+// Mongoose
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://192.168.0.30/controleDeVendas', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('Servidor mongo ATIVO')
+})
+
+// Rotas
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
+// Outras configurações
+const PORT = 3000
+app.listen(PORT, console.log('Servidor node ativo'))
---
+++ views/index.handlebars
@@ -0,0 +1,1 @@
+<h1>pagina inicial</h1>
