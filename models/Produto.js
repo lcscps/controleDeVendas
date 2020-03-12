d3d886929f8702b89433a19cde012f2a00e85dbb
Lucas do Nascimento <lucascps07@hotmail.com>
2020-03-11 21:07:14 -0300

    Inserido adição e listagem de produto

--- models/Produto.js
+++ models/Produto.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Produto = new Schema({
-
     descricao: {type: String, required: true},
     precoCusto: {type: Number, required: true},
     precoVenda: {type: Number, required: true},
--- routes/produtos.js
+++ routes/produtos.js
@@ -5,11 +5,32 @@ require('../models/Produto')
 const Produto = mongoose.model('produtos')
 
 router.get('/', (req,res) => {
-    res.render('produtos/home')
+    Produto.find().sort({descricao: 'ASC'}).then((produto) => {
+        res.render('produtos/home', {produto: produto})
+    }).catch((err) => {
+        req.flash('error_msg', 'Não foi possível listar os produtos')
+        res.redirect('/produtos')
+    })
 })
 
 router.get('/add', (req,res) => {
     res.render('produtos/addProduto')
+})
+
+router.post('/add', (req,res) => {
+    const novoProduto = {
+        descricao: req.body.descricao,
+        precoCusto: req.body.precoCusto,
+        precoVenda: req.body.precoVenda,
+        pesoOuMedida: req.body.pesoOuMedida
+    }
+    new Produto(novoProduto).save().then(() => {
+        req.flash('success_msg', 'Produto inserido com sucesso')
+        res.redirect('/produtos')
+    }).catch((err) => {
+        req.flash('error_msg', 'Produto não pode ser inserido. ERRO: '+err)
+        res.redirect('/produtos')
+    })
 })
 
 module.exports = router
