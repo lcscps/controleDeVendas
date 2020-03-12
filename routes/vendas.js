4a73903b393177526c3ed298ea72f9619235d38f
Lucas do Nascimento <lucascps07@hotmail.com>
2020-03-12 10:56:38 -0300

    Iniciado as views de venda e criado as rotas

--- routes/vendas.js
+++ routes/vendas.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Venda')
+require('../models/Produto')
+const Venda = mongoose.model('vendas')
+const Produto = mongoose.model('produtos')
 
+
+// LISTAR VENDAS
 router.get('/', (req,res) => {
-    res.render('vendas/home')
+    Venda.find().then((venda) => {
+        res.render('vendas/home', {venda: venda})
+    }).catch((err) => {
+        req.flash('error_msg', 'Não foi encontrado o registro de vendas. ERRO: '+ err)
+        res.redirect('/vendas')
+    })
 })
+
+// INSERIR VENDA
+router.get('/add', (req,res) => {
+    res.render('vendas/addVenda')
+})
+
+
 
 module.exports = router
