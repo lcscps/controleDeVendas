32a9551b8a92b6baffe27b3fc081a794254ebf72
Lucas do Nascimento <lucascps07@hotmail.com>
2020-03-12 10:47:56 -0300

    Finalizado o CRUD de Produtos

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,9 +1,11 @@
+// CARREGAR MODULOS
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/Produto')
 const Produto = mongoose.model('produtos')
 
+// LISTAR OS PRODUTOS CADASTRADOS
 router.get('/', (req,res) => {
     Produto.find().sort({descricao: 'ASC'}).then((produto) => {
         res.render('produtos/home', {produto: produto})
@@ -13,10 +15,33 @@ router.get('/', (req,res) => {
     })
 })
 
+// ADICIONAR NOVO PRODUTO
 router.get('/add', (req,res) => {
     res.render('produtos/addProduto')
 })
 
+// REMOVER PRODUTO
+router.get('/delete/:id', (req,res) => {
+    Produto.findOneAndDelete({_id: req.params.id}).then(() => {
+        req.flash('success_msg', 'Produto removido com sucesso')
+        res.redirect('/produtos')
+    }).catch((err) => {
+        req.flash('error_msg', 'Não foi posseivel localizar o produto')
+        res.redirect('/produtos')
+    })
+})
+
+// EDITAR PRODUTO (PUXAR OS DADOS PARA OS FORMULARIOS) 
+router.get('/edit/:id', (req,res) => {
+    Produto.findOne({_id: req.params.id}).then((produto) => {
+        res.render('produtos/editProduto', {produto: produto})
+    }).catch((err) => {
+        req.flash('error_msg', 'Produto não encontrado')
+        res.redirect('/produtos')
+    }) 
+})
+
+// POST INSERIR PRODUTOS
 router.post('/add', (req,res) => {
     const novoProduto = {
         descricao: req.body.descricao,
@@ -29,6 +54,27 @@ router.post('/add', (req,res) => {
         res.redirect('/produtos')
     }).catch((err) => {
         req.flash('error_msg', 'Produto não pode ser inserido. ERRO: '+err)
+        res.redirect('/produtos')
+    })
+})
+
+// POST SALVAR EDIÇÃO DO PRODUTO
+router.post('/editProduto', (req,res) => {
+    Produto.findOne({_id: req.body.id}).then((produto) => {
+        produto.descricao = req.body.descricao,
+        produto.precoCusto = req.body.precoCusto,
+        produto.precoVenda = req.body.precoVenda,
+        produto.pesoOuMedida = req.body.pesoOuMedida
+
+        produto.save().then(() => {
+            req.flash('success_msg', 'Produto alterado com sucesso')
+            res.redirect('/produtos')
+        }).catch((err) => {
+            req.flash('error_msg', 'Produto não pode ser alterado. Erro: '+err)
+            res.redirect('/produtos')
+        })
+    }).catch((err) => {
+        req.flash('error_msg', 'Produto não encontrado')
         res.redirect('/produtos')
     })
 })
