3cd17a70d4b6ca60492c4df3fa4e0f7166520d6b
Lucas do Nascimento <lucascps07@hotmail.com>
2020-03-15 11:06:20 -0300

    Finalizado a criação das views de vendas e suas rotas

--- models/Venda.js
+++ models/Venda.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Venda = new Schema({
-
+    data: {type: Date, required: true},
+    produto: {type: Schema.Types.ObjectId, ref: 'produtos', required: true},
+    quantidade: {type: Number, require: true}
 })
 mongoose.model('vendas', Venda) 
--- routes/vendas.js
+++ routes/vendas.js
@@ -6,20 +6,69 @@ require('../models/Produto')
 const Venda = mongoose.model('vendas')
 const Produto = mongoose.model('produtos')
 
+// EDITAR VENDAS
+router.get('/edit/:id', (req,res) => {
+    Venda.findOne({_id: req.params.id}).then((venda) => {
+        Produto.find().then((produto) => {
+            res.render('vendas/editVenda', {venda: venda, produto: produto})
+        })
+    })
+})
 
-// LISTAR VENDAS
-router.get('/', (req,res) => {
-    Venda.find().then((venda) => {
-        res.render('vendas/home', {venda: venda})
-    }).catch((err) => {
-        req.flash('error_msg', 'Não foi encontrado o registro de vendas. ERRO: '+ err)
+// Remover venda
+router.get('/delete/:id', (req,res) => {
+    Venda.findOneAndDelete({_id: req.params.id}).then(() => {
+        req.flash('success_msg', 'Venda removida com sucesso')
+        res.redirect('/vendas')
+    }).catch((err) =>{
+        req.flash('error_msg', 'Venda não pode ser removida, ERRO: '+err)
         res.redirect('/vendas')
     })
 })
 
+
+// LISTAR VENDAS
+router.get('/', (req, res) => {
+    Venda.find().populate('produto').then((venda) => {
+        res.render('vendas/home', {venda: venda})
+    })
+})
+
 // INSERIR VENDA
-router.get('/add', (req,res) => {
-    res.render('vendas/addVenda')
+router.get('/add', (req, res) => {
+    Produto.find().then((produto) => {
+        res.render('vendas/addVenda', { produto: produto })
+    })
+})
+
+// POST EDITAR VENDA
+router.post('/editVenda',(req,res) => {
+    Venda.findOne({_id: req.body.id}).then((venda) => {
+        venda.produto = req.body.produto,
+        venda.data = req.body.data,
+        venda.quantidade = req.body.quantidade
+
+        venda.save().then(() => {
+            req.flash('success_msg', 'Venda editada com sucesso')
+            res.redirect('/vendas')
+        }).catch((err) => {
+            req.flash('error_msg', 'Venda não pode ser editada')
+            res.redirect('/vendas')
+        })
+    })
+})
+
+//  POST INSERIR VENDA
+router.post('/add', (req, res) => {
+    const novaVenda = {
+        produto: req.body.produto,
+        data: req.body.data,
+        quantidade: req.body.quantidade
+    }
+    new Venda(novaVenda).save().then(() => {
+        req.flash('success_msg', 'Venda registrada com sucesso')
+        res.redirect('/vendas')
+    })
 })
 
 
